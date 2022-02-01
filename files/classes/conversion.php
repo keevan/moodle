bc725398c449db851f74a744701c3e758ee35517
Kevin Pham <keevan.pham@gmail.com>
2022-02-01 17:04:40 +1100

    MDL-73727 core_files: Formatted the query to meet SQL coding styles

--- files/classes/conversion.php
+++ files/classes/conversion.php
@@ -129,14 +129,16 @@ class conversion extends \core\persistent {
         // Fetch actual conversions which relate to the specified source file, and have a matching conversion record,
         // and either have a valid destination file which still exists, or do not have a destination file at all.
         $sql = "SELECT {$sqlfields}
-                FROM {" . self::TABLE . "} c
-                INNER JOIN (SELECT id FROM {files} WHERE contenthash = :contenthash LIMIT 1) conversionsourcefile ON conversionsourcefile.id = c.sourcefileid
-                LEFT JOIN {files} conversiondestfile ON conversiondestfile.id = c.destfileid
-                WHERE
-                    c.targetformat = :cformat
-                AND (
-                    c.destfileid IS NULL OR conversiondestfile.id IS NOT NULL
-                )";
+                  FROM {" . self::TABLE . "} c
+            INNER JOIN (SELECT id
+                          FROM {files}
+                         WHERE contenthash = :contenthash
+                         LIMIT 1
+                       ) conversionsourcefile ON conversionsourcefile.id = c.sourcefileid
+             LEFT JOIN {files} conversiondestfile ON conversiondestfile.id = c.destfileid
+                 WHERE c.targetformat = :cformat
+                       AND (c.destfileid IS NULL
+                            OR conversiondestfile.id IS NOT NULL)";
 
         // Fetch a empty conversion record for each source/destination combination that we find to match where the
         // destination file is in the correct filearea/filepath/filename combination to meet the requirements.
